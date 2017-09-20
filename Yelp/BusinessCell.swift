cd6a917f4d933a82565b06c52910acd6cf73cbe9
Diana Fisher <diana.fisher@gmail.com>
2017-09-20 14:57:28 -0700

    Begin building user interface.

---
+++ Yelp/BusinessCell.swift
@@ -0,0 +1,51 @@
+//
+//  BusinessCell.swift
+//  Yelp
+//
+//  Created by Diana Fisher on 9/20/17.
+//  Copyright © 2017 Timothy Lee. All rights reserved.
+//
+
+import UIKit
+
+class BusinessCell: UITableViewCell {
+
+    @IBOutlet weak var thumbImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var distanceLabel: UILabel!
+    @IBOutlet weak var ratingImageView: UIImageView!
+    @IBOutlet weak var reviewCountLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var categoriesLabel: UILabel!
+    
+    var business: Business! {
+        didSet {
+            nameLabel.text = business.name
+            thumbImageView.setImageWith(business.imageURL!)
+            categoriesLabel.text = business.categories
+            addressLabel.text = business.address
+            reviewCountLabel.text = "\(business.reviewCount!) Reviews"
+            ratingImageView.setImageWith(business.ratingImageURL!)
+            distanceLabel.text = business.distance
+        }
+    }
+    
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        
+        // round the corners of the UIImageView
+        thumbImageView.layer.cornerRadius = 3
+        thumbImageView.clipsToBounds = true
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
